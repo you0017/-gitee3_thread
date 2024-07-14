4c4a8085d7d68fa52d9b3e601e38dc9590faeeee
you0017 <2092425375@qq.com>
2024-07-14 09:32:28 +0800

    第一次提交

---
+++ src/main/java/com/yc/thread/Test2_Runnable.java
@@ -0,0 +1,17 @@
+package com.yc.thread;
+
+/**
+ * Runnable可运行的任务
+ */
+public class Test2_Runnable {
+}
+
+
+//从任务角度来看
+class ShowTimeTask implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
