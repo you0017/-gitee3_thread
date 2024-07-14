9d6fc8ea98ae800df13851ed63f9fd469a2e4c78
you0017 <2092425375@qq.com>
2024-07-14 09:03:01 +0800

    第一次提交

--- src/main/java/com/yc/App.java
+++ src/main/java/com/yc/App.java
@@ -6,8 +6,13 @@ package com.yc;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws InterruptedException {
+        show();
+    }
+
+
+    public static void show() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("2");
     }
 }
